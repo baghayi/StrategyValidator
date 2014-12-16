86506401d358d09cfb00480e4a2cfba26f24c482
Hossein Baghayi <hossein.baghayi@gmail.com>
2014-12-16 10:06:49 +0330

    Making Strategy validator available to Apigility
    So that it could be selected and used via Apigility admin page.

---
+++ config/apigility.config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'validator_metadata' => array(
+        'StrategyValidator\Validator\Strategy' => array(
+            'strategy' => 'string',
+            'message' => 'string',
+        ),
+    ),
+);
