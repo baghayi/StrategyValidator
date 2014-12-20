6fc65e7f2c4775aa847c420a4f85b61d011a1060
Hossein Baghayi <hossein.baghayi@gmail.com>
2014-12-20 06:38:36 +0330

    Created classmap file

---
+++ autoload_classmap.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+  'StrategyValidator\Strategy\StrategyInterface' => __DIR__ . '/src/StrategyValidator/Strategy/StrategyInterface.php',
+  'StrategyValidator\Validator\Strategy'         => __DIR__ . '/src/StrategyValidator/Validator/Strategy.php',
+  'StrategyValidator\Validator\StrategyFactory'  => __DIR__ . '/src/StrategyValidator/Validator/StrategyFactory.php',
+  'StrategyValidator\Exception\StrategyNotFound' => __DIR__ . '/src/StrategyValidator/Exception/StrategyNotFound.php',
+  'StrategyValidator\Module'                     => __DIR__ . '/Module.php',
+);
