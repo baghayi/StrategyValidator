7be1c6939ad4f29924e5d70f08821ede012b35b8
Hossein Baghayi <hossein.baghayi@gmail.com>
2014-12-20 05:30:17 +0330

    Add docblock for specifying input and return type

--- src/StrategyValidator/Strategy/StrategyInterface.php
+++ src/StrategyValidator/Strategy/StrategyInterface.php
@@ -3,6 +3,11 @@ namespace StrategyValidator\Strategy;
 
 interface StrategyInterface {
 
+    /**
+    * 
+    * @param $value Value or content to be validated.
+    * @return boolean
+    */
     public function isValid($value);
 
 }
